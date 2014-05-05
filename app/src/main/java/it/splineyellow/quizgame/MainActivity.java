41d67fc6b7253352c6020287063219e606023306
Federico Bertoli <thebertozz@gmail.com>
2014-05-05 14:14:01 +0200

    UDP connection tests, removed UtentiDatabaseAdapter

--- app/src/main/java/it/splineyellow/quizgame/MainActivity.java
+++ app/src/main/java/it/splineyellow/quizgame/MainActivity.java
@@ -1,36 +1,110 @@
 package it.splineyellow.quizgame;
 
 import android.app.Activity;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.client.HttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends Activity {
+
+    TextView textResponse;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextAddress = (EditText)findViewById(R.id.address);
+        editTextPort = (EditText)findViewById(R.id.port);
+        buttonConnect = (Button)findViewById(R.id.connect);
+        buttonClear = (Button)findViewById(R.id.clear);
+        textResponse = (TextView)findViewById(R.id.response);
+
+        buttonConnect.setOnClickListener(buttonConnectOnClickListener);
+
+        buttonClear.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                textResponse.setText("");
+            }});
     }
 
+    View.OnClickListener buttonConnectOnClickListener =
+            new View.OnClickListener(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+                @Override
+                public void onClick(View arg0) {
+                    MyClientTask myClientTask = new MyClientTask(
+                            editTextAddress.getText().toString(),
+                            Integer.parseInt(editTextPort.getText().toString()));
+                    myClientTask.execute();
+                }};
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+
+        MyClientTask(String addr, int port){
+            dstAddress = addr;
+            dstPort = port;
         }
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            DatagramSocket ds = null;
+
+            try
+            {
+                InetAddress serverAddr = InetAddress.getByName(dstAddress);
+                String data = "Sconosciuto,pass";
+                byte[] buffer = data.getBytes();
+                ds = new DatagramSocket();
+                DatagramPacket dp;
+                dp = new DatagramPacket(buffer, buffer.length, serverAddr, dstPort);
+                ds.setBroadcast(true);
+                ds.send(dp);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (ds != null)
+                {
+                    ds.close();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            super.onPostExecute(result);
+        }
+
     }
+
 }
