ce0835165933943e6a9a00a5f4493af0dd5e58ed
Federico Bertoli <thebertozz@gmail.com>
2014-05-09 20:25:12 +0200

    Added freakin' awesome gridView to StartGameActivity, for a smooth and tasty experience

--- app/src/main/java/it/splineyellow/quizgame/StartGameActivity.java
+++ app/src/main/java/it/splineyellow/quizgame/StartGameActivity.java
@@ -1,20 +1,44 @@
 package it.splineyellow.quizgame;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StartGameActivity extends Activity {
+
+    public static final String TAG = "onItemClick --> posizione : ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
 
-        /*
-            Set Activity Title.
-         */
         setTitle("Nuova Partita");
+
+        TextView textView = (TextView) findViewById(R.id.placeholder);
+        textView.setText("Placeholder Textview, Punteggi, Nomi giocatori");
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Log.d(TAG, "" + position);
+                //Starts from 0
+                Toast.makeText(StartGameActivity.this, "Elemento in posizione: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -48,4 +72,46 @@ public class StartGameActivity extends Activity {
 
         return super.onPrepareOptionsMenu(menu);
     }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {  // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        private Integer[] mThumbIds = {R.drawable.arte, R.drawable.cinema, R.drawable.geografia,
+                R.drawable.informatica, R.drawable.letteratura, R.drawable.matematica,
+                R.drawable.musica, R.drawable.sport, R.drawable.storia};
+
+    }
+
 }
