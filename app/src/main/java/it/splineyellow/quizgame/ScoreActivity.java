c35288b36a43e09d55393d44a738c99b7c997c4d
ftrombi <francesco.trombi@outlook.com>
2014-05-27 16:20:01 +0200

    Added SendScoreTask

--- app/src/main/java/it/splineyellow/quizgame/ScoreActivity.java
+++ app/src/main/java/it/splineyellow/quizgame/ScoreActivity.java
@@ -2,8 +2,10 @@ package it.splineyellow.quizgame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,12 +13,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.sql.SQLException;
 
 
 public class ScoreActivity extends Activity {
 
     public final static String EXTRA_MESSAGE = "it.splineyellow.quizgame.MESSAGE";
+    public final static String dstAddress = "thebertozz.no-ip.org";
+    public final static int dstPort = 9533;
+
+    public String score;
 
     UtentiDatabaseAdapter db = new UtentiDatabaseAdapter(this);
     String message;
@@ -28,7 +37,7 @@ public class ScoreActivity extends Activity {
         setContentView(R.layout.score_activity);
 
         Intent intent = getIntent();
-        String score = intent.getStringExtra(QuestionActivity.EXTRA_MESSAGE);
+        score = intent.getStringExtra(QuestionActivity.EXTRA_MESSAGE);
         message = intent.getStringExtra("Categories");
 
         TextView textScore = (TextView) findViewById(R.id.score);
@@ -56,10 +65,36 @@ public class ScoreActivity extends Activity {
 
     public void goToStartGameActivity () {
 
-        Intent intent = new Intent(this, StartGameActivity.class);
+        new SendScoreTask().execute();
 
+        Intent intent = new Intent(this, StartGameActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
 
         startActivity(intent);
+    }
+
+    public class SendScoreTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            DatagramSocket ds = null;
+            try {
+                InetAddress serverAddr = InetAddress.getByName(dstAddress);
+                byte[] buffer = score.getBytes();
+                ds = new DatagramSocket();
+                DatagramPacket dp;
+                dp = new DatagramPacket(buffer, buffer.length, serverAddr, dstPort);
+                ds.send(dp);
+                Log.v("Send score", "inviato punteggio " + score);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ds.close();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
     }
 }
